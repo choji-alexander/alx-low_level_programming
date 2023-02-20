e6b74fdd174a793e5bb0a4e5cbe8679f83808643
choji-alexander <chojialexander@gmail.com>
2023-02-20 05:53:11 -0800

    initials

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,20 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0){
-	printf("%i is positive /n", n);}
-	else if (n == 0){
-	printf("%i is zero /n", n);}
-	else if(n < 0){
-	printf("%i is negative /n", n);}
+	if (n > 0)
+	{
+	printf("%i is positive", n);
+	}
+	else if (n == 0)
+	{
+	printf("%i is zero", n);
+	}
+	else if (n < 0)
+	{
+	printf("%i is negative", n);
+	}
 	return (0);
 }
