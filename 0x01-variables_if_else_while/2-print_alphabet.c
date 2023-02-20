80bc36a54966a1a23a8e760e3cd69fb1475495ab
choji-alexander <chojialexander@gmail.com>
2023-02-20 07:15:54 -0800

    commit3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	int ch;
+
+	for (ch= ‘a’; ch<= ‘z’; ch++)
+
+                  putchar(ch);
+
+                  putchar (‘\n’) ;
+
+                  getch();
+}
