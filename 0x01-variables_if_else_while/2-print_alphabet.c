0f61359fbb864fb29f394e3951b72d895aab7cff
choji-alexander <chojialexander@gmail.com>
2023-02-20 07:40:53 -0800

    commit3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int ch;
+	char letter;
 
-	for (ch= 'a'; ch<= 'z'; ch++)
-	{
-		putchar(ch);
-		putchar ('\n'); 
-	}
-	getch();
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
